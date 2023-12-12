ca46c746cf16421c42c1f03364c86fce62e3c6b7
Dirk Eddelbuettel <edd@debian.org>
2023-12-12 07:49:43 -0600

    Switch to tinythemes, step function, simpler factor creation

--- scripts/top50chart.R
+++ scripts/top50chart.R
@@ -4,7 +4,7 @@ suppressMessages({
     library(data.table)
     library(anytime)
     library(ggplot2)
-    library(hrbrthemes)
+    library(tinythemes)
 })
 
 D <- readRDS("data/top50.rds")
@@ -12,24 +12,24 @@ setDT(D)
 
 D[, song_artist := paste0(artists, "\n", name)] 	# prettier song + artist string
 D[, dt := anytime::anydate(date)]                       # use just the date
+
 D2w <- D[dt >= max(dt)-14,]                             # subset to last two weeks
 D2w[, meanpos := mean(pos), by=song_artist]             # compute 'average' position
 D2w[, cnt := nrow(.SD), by=name]                        # count of Top50 appearances
 
-song_artist_sorted <- D2w[, .(meanpos=unique(meanpos)), by=song_artist][order(meanpos),song_artist]
-D2w[, song_artist := factor(as.character(song_artist),  # make song_artist an ordered factor
-                            levels = song_artist_sorted,
-                            ordered = TRUE)]
-crit <- max(head(unique(D2w[order(meanpos),meanpos]), 9)) # cut-off to include top nine
+                                                        # ensure song_artist is ordered factor
+D2w <- D2w[order(meanpos),][, song_artist := ordered(song_artist, levels=unique(song_artist))]
+
+crit <- D2w[order(meanpos), unique(meanpos)][9] 	# cut-off to include top nine
 
 p <- ggplot(data=D2w[meanpos <= crit]) +
     aes(x=dt, y=pos, colour=song_artist) +
-    geom_line(linewidth=1.25) +
+    geom_step(linewidth=1.0) +
     facet_wrap(~ song_artist) +
-    hrbrthemes::theme_ipsum_rc() +
+    scale_y_reverse() +
+    tinythemes::theme_ipsum_rc() +
     theme(legend.position="none") +
     xlab("Date") + ylab("Top 50 Position") +
-    scale_y_reverse() +
     labs(title = "Spotify 'Top 50' in the US: Top Nine Songs in the Last Fourteen Days",
          subtitle = paste("Songs ranked by average 'Top 50' position over last two",
                           "weeks up to and including", format(D2w[, max(dt)], "%B %d")),
