b22295003b7fc05796773bc777336dddbb92eeb7
Dirk Eddelbuettel <edd@debian.org>
2023-12-13 07:06:46 -0600

    Use minimum appearance of four song, set UTC time

--- scripts/top50chart.R
+++ scripts/top50chart.R
@@ -7,20 +7,21 @@ suppressMessages({
     library(tinythemes)
 })
 
+Sys.setenv("TZ"="UTC") 				# GitHub Actions runs at UTC too
 D <- readRDS("data/top50.rds")
 setDT(D)
 
-D[, song_artist := paste0(artists, "\n", name)] 	# prettier song + artist string
-D[, dt := anytime::anydate(date)]                       # use just the date
+D[, song_artist := paste0(artists, "\n", name)] # prettier song + artist string
+D[, dt := anytime::anydate(date)]               # use just the date
 
-D2w <- D[dt >= max(dt)-14,]                             # subset to last two weeks
-D2w[, meanpos := mean(pos), by=song_artist]             # compute 'average' position
-D2w[, cnt := nrow(.SD), by=name]                        # count of Top50 appearances
+D2w <- D[dt >= max(dt)-14,]                     # subset to last two weeks
+D2w[, meanpos := mean(pos), by=song_artist]     # compute 'average' position
+D2w[, cnt := .N, by=song_artist]            	# count of Top50 appearances
 
-                                                        # ensure song_artist is ordered factor
-D2w <- D2w[order(meanpos),][, song_artist := ordered(song_artist, levels=unique(song_artist))]
+D2w <- D2w[cnt >= 4][order(meanpos),]		# subset, sort, ensure song_artist is ordered factor
+D2w[, song_artist := ordered(song_artist, levels=unique(song_artist))]
 
-crit <- D2w[order(meanpos), unique(meanpos)][9] 	# cut-off to include top nine
+crit <- D2w[order(meanpos), unique(meanpos)][9] # cut-off to include top nine
 
 p <- ggplot(data=D2w[meanpos <= crit]) +
     aes(x=dt, y=pos, colour=song_artist) +
@@ -32,11 +33,12 @@ p <- ggplot(data=D2w[meanpos <= crit]) +
     xlab("Date") + ylab("Top 50 Position") +
     labs(title = "Spotify 'Top 50' in the US: Top Nine Songs in the Last Fourteen Days",
          subtitle = paste("Songs ranked by average 'Top 50' position over last two",
-                          "weeks up to and including", format(D2w[, max(dt)], "%B %d")),
+                          "weeks up to and including", format(D2w[, max(dt)], "%B %d"),
+                          "and subject to at least 4 appearances"),
          caption = paste("Initial data from https://github.com/brianckeegan/SpotifyUSTop50/; ",
                          "more recent data and graphics from",
                          "https://github.com/eddelbuettel/spotifytop50us.",
-                         "Chart last updated at", format(round(Sys.time(),0))))
+                         "Chart last updated at", format(round(Sys.time(),0)), "UTC"))
 
 png("graphs/top50us.png", 1200, 600)
 p
