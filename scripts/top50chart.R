9b7a940c5cb35d3056557fb787fee3f3b4c3dc0f
Dirk Eddelbuettel <edd@debian.org>
2023-12-31 08:46:30 -0600

    Chart generator now a function of 7, 14, 21 (default) or 28 days

--- scripts/top50chart.R
+++ scripts/top50chart.R
@@ -14,34 +14,40 @@ setDT(D)
 D[, song_artist := paste0(artists, "\n", name)] # prettier song + artist string
 D[, dt := anytime::anydate(date)]               # use just the date
 
-D2w <- D[dt > max(dt) - 21,]                    # subset to last three weeks
-D2w[, meanpos := mean(pos), by=song_artist]     # compute 'average' position
-D2w[, cnt := .N, by=song_artist]            	# count of Top50 appearances
+makeChart <- function(days = 21) {
+    days <- match.arg(as.character(days), c("7", "14", "21", "28"))
+    D2 <- D[dt > max(dt) - as.integer(days),]   # subset to selected window
+    D2[, meanpos := mean(pos), by=song_artist]  # compute 'average' position
+    D2[, cnt := .N, by=song_artist]             # count of Top50 appearances
 
-D2w <- D2w[cnt >= 4][order(meanpos),]		# subset, sort, ensure song_artist is ordered factor
-D2w[, song_artist := ordered(song_artist, levels=unique(song_artist))]
+    D2 <- D2[cnt >= 4][order(meanpos),]		# subset, sort, ensure song_artist is ordered factor
+    D2[, song_artist := ordered(song_artist, levels=unique(song_artist))]
 
  						# cut-off to include top nine, allowing ties
-crit <- D2w[order(meanpos), .(meanpos=head(meanpos,1)), by=song_artist][1:9, max(meanpos)]
+    crit <- D2[order(meanpos), .(meanpos=head(meanpos,1)), by=song_artist][1:9, max(meanpos)]
+    ndays <- switch(days, "7" = "Seven", "14" = "Fourteen", "21" = "Twentyone", "28" = "Twentyeight")
+    nweeks <- switch(days, "7" = "one week", "14" = "two weeks", "21" = "three weeks", "28" = "four weeks")
 
-p <- ggplot(data=D2w[meanpos <= crit]) +
-    aes(x=dt, y=pos, colour=song_artist) +
-    geom_step(linewidth=1.0) +
-    facet_wrap(~ song_artist) +
-    scale_y_reverse() +
-    tinythemes::theme_ipsum_rc() +
-    theme(legend.position="none") +
-    xlab("Date") + ylab("Top 50 Position") +
-    labs(title = "Spotify 'Top 50' in the US: Top Nine Songs in the Last Twentyone Days",
-         subtitle = paste("Songs ranked by average 'Top 50' position over last three",
-                          "weeks up spanning", format(D2w[, min(dt)], "%B %d"),
-                          "to", format(D2w[, max(dt)], "%B %d"),
-                          "and subject to at least 4 appearances"),
-         caption = paste("Initial data from https://github.com/brianckeegan/SpotifyUSTop50/; ",
-                         "more recent data and graphics from",
-                         "https://github.com/eddelbuettel/spotifytop50us.",
-                         "Chart last updated at", format(round(Sys.time(),0)), "UTC"))
+    p <- ggplot(data=D2[meanpos <= crit]) +
+        aes(x=dt, y=pos, colour=song_artist) +
+        geom_step(linewidth=1.0) +
+        facet_wrap(~ song_artist) +
+        scale_y_reverse() +
+        tinythemes::theme_ipsum_rc() +
+        theme(legend.position="none") +
+        xlab("Date") + ylab("Top 50 Position") +
+        labs(title = paste("Spotify 'Top 50' in the US: Top Nine Songs in the Last", ndays, "Days"),
+             subtitle = paste("Songs ranked by average 'Top 50' position over last", nweeks,
+                              "spanning", format(D2[, min(dt)], "%B %d"), "to", format(D2[, max(dt)], "%B %d"),
+                              "and subject to at least 4 appearances"),
+             caption = paste("Initial data from https://github.com/brianckeegan/SpotifyUSTop50/; ",
+                             "more recent data and graphics from",
+                             "https://github.com/eddelbuettel/spotifytop50us.",
+                             "Chart last updated at", format(round(Sys.time(),0)), "UTC"))
+    p
+}
 
+p <- makeChart()
 png("graphs/top50us.png", 1200, 600)
 p
 ignoreme <- dev.off()
