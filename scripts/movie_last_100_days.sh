86761600e737099d57a8a5b4ade6fb63e49012db
Dirk Eddelbuettel <edd@debian.org>
2024-04-23 07:06:35 -0500

    Refine git checkout

--- scripts/movie_last_100_days.sh
+++ scripts/movie_last_100_days.sh
@@ -5,7 +5,9 @@ if [ ! -d ${graphdir} ]; then
     mkdir ${graphdir}
 fi
 
-N=100
+## We retrieve the last 99 committed ones -- plus the current (not yet committed) one
+N=99
+cp -vax graphs/top50us.png ${graphdir}/top50us_1100.png
 
 ## get all commits, use top N, and reverse
 commits=$(git log --oneline graphs/top50us.png | awk '{print $1}' | head -${N} | tac)
@@ -13,10 +15,10 @@ commits=$(git log --oneline graphs/top50us.png | awk '{print $1}' | head -${N} |
 ## start at 1000 to always have four digits, also count backwards to reverse order of commits
 counter=1000
 for cmt in ${commits}; do
-    git checkout ${cmt}
+    git checkout ${cmt} -- graphs/top50us.png
     ## we could use git log to get the date of the commit but the files are really tempfiles so ...
     counter=$(expr ${counter} + 1)
-    cp graphs/top50us.png ${graphdir}/top50us_${counter}.png
+    cp -vax graphs/top50us.png ${graphdir}/top50us_${counter}.png
 done
 
 ## we use 'gm convert' from graphicsmagick (formerly imagemagick)
@@ -25,5 +27,6 @@ gm convert -delay 20 ${graphdir}/*.png graphs/movie_last_n_days.gif
 
 rm -rf ${graphdir}
 
-git checkout master
-git pull --all
+## not really needed in github action: restore to last commit of chart
+git restore --staged graphs/top50us.png
+git restore graphs/top50us.png
